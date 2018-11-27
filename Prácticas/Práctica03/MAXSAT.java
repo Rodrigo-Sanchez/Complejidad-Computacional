c88c286c7b90f770e17c855a8f8590a7a75d2746
Rodrigo-Sanchez <rodrigosanchez@ciencias.unam.mx>
2018-11-27 13:11:11 -0600

    Agregando la asignación de verdad.

--- Prácticas/Práctica03/MAXSAT.java
+++ Prácticas/Práctica03/MAXSAT.java
@@ -1,25 +1,31 @@
 import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.Random;
+// import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class MAXSAT {
-    
+
     public static void main(String args[]) {
         System.out.println("Algoritmo Genético para MAXSAT.\n");
 
         int[][] initialPopulation = createPopulation();
-        ArrayList<Integer> array[] = createLists(initialPopulation[0].length);
+        ArrayList<Integer> array[] = createLists(initialPopulation.length);
         for(int i = 0; i < initialPopulation.length; i++) {
+            // System.out.println(initialPopulation.length);
+            // System.out.println(initialPopulation[0].length);
             for(int j = 0; j < initialPopulation[0].length; j++) {
+                // System.out.println("i: "+i+" j: "+j);
                 array[i].add(initialPopulation[i][j]);
             }
         }
-        System.out.println(Arrays.deepToString(array));
+        //System.out.println(Arrays.deepToString(array));
         //printMatrix(initialPopulation);
-        //printClausules(initialPopulation);
+        printClausules(initialPopulation);
+        ArrayList<Integer> list = createTruthAssignment(initialPopulation[0].length);
+        Arrays.toString(list.toArray());
     }
+
 
     public static int[][] createPopulation() {
         // Random random = new Random();
@@ -33,6 +39,9 @@ public class MAXSAT {
 
         // Cantidad de variables.
         int n = ThreadLocalRandom.current().nextInt(100)+1;
+
+        // Random para determinar si es positivo o negativo.
+        int paridad;
 
         int[][] population = new int[m][n];
 
@@ -53,9 +62,11 @@ public class MAXSAT {
             }
 
             for(int j = 0; j < population[0].length; j++) {
-                // Si está la variable, agregamos un 1, en otro caso un 0.
-                population[i][j] = positions.contains(j) ? j+1 : -(j+1);
-                //population[i][j] = positions.contains(j) ? 1 : 0;
+                paridad = ThreadLocalRandom.current().nextInt(2);
+                // Si está la variable, agregamos la posición ya sea en positivo o negativo, en otro caso un 0.
+                population[i][j] = positions.contains(j) ? (paridad > 0 ? j+1 : -(j+1)) : 0;
+                // population[i][j] = positions.contains(j) ? j+1 : -(j+1);
+                // population[i][j] = positions.contains(j) ? 1 : 0;
             }
 
             // Al finalizar esta iteración limpiamos el arreglo donde guardamos las posiciones.
@@ -64,6 +75,7 @@ public class MAXSAT {
 
         return population;
     }
+
 
     public static void printMatrix(int[][] matrix) {
         Arrays.stream(matrix)
@@ -77,18 +89,18 @@ public class MAXSAT {
         );
     }
 
+
     public static void printClausules(int[][] matrix) {
         for(int i = 0; i < matrix.length; i++) {
-            // System.out.println("Cláusula "+i+": ");
-            System.out.print("(");
+            System.out.print("Cláusula "+ (i+1) +": (");
             for(int j = 0; j < matrix[0].length; j++) {
                 if(matrix[i][j] > 0) {
                     System.out.print("x" + matrix[i][j] + " ∨ ");
-                } else {
-                    System.out.print("¬x" + Math.abs(matrix[i][j]) + " ∨ ");
+                } else if (matrix[i][j] < 0) {
+                    System.out.print("¬x" + Math.abs(matrix[i][j]) + " ∧ ");
                 }
             }
-            System.out.println(") ∧ ");
+            System.out.println(") ∨ ");
         }
         // Arrays.stream(matrix)
         // .forEach(
@@ -107,16 +119,25 @@ public class MAXSAT {
     }
 
     /**
-     * Crea t arreglos de enteros.
-     * @param t numero de arreglos a crear.
+     * Crea n arreglos de enteros.
+     * @param n numero de arreglos a crear.
      * @return Lista de arreglos.
      */
-    public static ArrayList<Integer>[] createLists(int t) {
-        ArrayList<Integer> list[] = new ArrayList[t];
-        for(int i = 0; i < t; i++) {
+    public static ArrayList<Integer>[] createLists(int n) {
+        ArrayList<Integer> list[] = new ArrayList[n];
+        for(int i = 0; i < n; i++) {
             list[i] = new ArrayList<Integer>();
         }
         return list;
     }
 
+    public static ArrayList<Integer> createTruthAssignment(int n) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int random;
+        for(int x : list) {
+            random = ThreadLocalRandom.current().nextInt(2);
+            list.add(random);
+        }
+        return list;
+    }
 }
